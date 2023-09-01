2d4acece85daed9ed478c3c56b3d5422fd58203d
Reljod Oreta <oretareljod@gmail.com>
2023-09-01 22:17:08 +0800

    create greetings sample

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/Reljod/tw-diary-api-service/greetings"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,14 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		message := greetings.Hello(name)
+		c.JSON(http.StatusOK, gin.H{
+			"message": message,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
